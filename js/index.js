9213edd41686cd7e626b4ae5486a7bf1e66793fe
ingrid-ca <and04688@umn.edu>
2023-03-01 22:29:00 -0600

    some work

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("anything");
+function down1(){
+    document.getElementById("about").scrollIntoView();
+}
+function down2(){
+    document.getElementById("skills").scrollIntoView();
+}
+function down3(){
+    document.getElementById("work").scrollIntoView();
+}
+function down4(){
+    document.getElementById("contact").scrollIntoView();
+}
